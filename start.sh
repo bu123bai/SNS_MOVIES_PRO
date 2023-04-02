2e598ae703de2732127dc301fffa58b4060cb67f
senapatibubai111 <120000603+senapatibubai111@users.noreply.github.com>
2023-04-02 15:26:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/senapatibubai111/DQ_FONT_TG.git /DQ_FONT_TG
+  git clone https://github.com/senapatibubai111/SNS_MOVIES_PRO.git /SNS_MOVIES_PRO
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ_FONT_TG
+  git clone $UPSTREAM_REPO /SNS_MOVIES_PRO
 fi
-cd /DQ_FONT_TG
+cd /SNS_MOVIES_PRO
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
