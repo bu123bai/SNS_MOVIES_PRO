dafbee0f74a6177706a94f2dfe4a100d355381d8
bu123bai <120026811+bu123bai@users.noreply.github.com>
2023-05-02 10:27:34 +0530

    Update commands.py

--- plugin/commands.py
+++ plugin/commands.py
@@ -252,7 +252,7 @@ async def start(client, message):
         try:
             if not await check_verification(client, message.from_user.id) and VERIFY == True:
                 btn = [[
-                    InlineKeyboardButton("💠 𝗖𝗹𝗶𝗰𝗸 𝗧𝗼 𝗩𝗲𝗿𝗶𝗳𝘆 💠", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
+                    InlineKeyboardButton("💠 𝗖𝗹𝗶𝗰𝗸 𝗧𝗼 𝗩𝗲𝗿𝗶𝗳𝘆 💠", url=await get_token(client, message.from_user.id, f"https://telegram.me/{clonedme.U_NAME}?start="))
                 ]]
                 await message.reply_text(
                     text="<b>Hey, You are not verified !\nKindly verify yourself to get unlimited access !</b>",
@@ -304,7 +304,7 @@ async def start(client, message):
         f_caption = f"{files.file_name}"
     if not await check_verification(client, message.from_user.id) and VERIFY == True:
         btn = [[
-            InlineKeyboardButton("💠 𝗖𝗹𝗶𝗰𝗸 𝗧𝗼 𝗩𝗲𝗿𝗶𝗳𝘆 💠", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
+            InlineKeyboardButton("💠 𝗖𝗹𝗶𝗰𝗸 𝗧𝗼 𝗩𝗲𝗿𝗶𝗳𝘆 💠", url=await get_token(client, message.from_user.id, f"https://telegram.me/{clonedme.U_NAME}?start="))
         ]]
         await message.reply_text(
             text="<b>Hey, You are not verified !\nKindly verify yourself to get unlimited access !</b>",
